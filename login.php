81f66469130d48b80092607ea7fc6ef6c571cd8d
Princessofthebloodymoon <EF.freedom16@gmail.com>
2023-02-01 15:59:58 +0200

    First commit

---
+++ login.php
@@ -0,0 +1,22 @@
+
+    <div class="container my-5">
+        <h1 class="text-center my-3">Авторизация на сайте</h1>
+        <div class="col-sm-5 mx-auto">
+            <form action="/localhost/handlerLogin.php" method="post">
+                <div class="mb-3">
+                    <input name="email" type="email" class="form-control" placeholder="E-mail">
+                </div>
+                <div class="mb-3">
+                    <input name="pass" type="password" class="form-control" placeholder="Пароль">
+                </div>
+                <p style="color: red;">
+                    <?php if($_GET['m'] == 'error'): ?>
+                    Неправильный логин или пароль
+                    <?php endif; ?>
+                </p>
+                <div class="mb-3">
+                    <input type="submit" class="form-control btn btn-primary" value="Войти">
+                </div>
+            </form>
+        </div>
+    </div>
