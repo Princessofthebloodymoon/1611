5fbdc9bab23156674d51f412c3f0c7a1c301210f
Princessofthebloodymoon <EF.freedom16@gmail.com>
2023-02-04 17:04:31 +0200

    First commit

--- handlerReg.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $name = $_POST['name'];
-    $lastname = $_POST['lastname'];
-    $email = $_POST['email'];
-    $pass = $_POST['pass'];
-    echo $name.$lastname.$email.$pass;
-    $mysqli = new mysqli("localhost", "root", "", "php1611");
-    $mysqli->query("INSERT INTO `users`(`name`, `lastname`, `email`, `pass`) VALUES ('$name','$lastname','$email','$pass')");
